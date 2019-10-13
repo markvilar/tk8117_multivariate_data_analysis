5e6b06d3ac6ce1e399d8aae8f5173f4d630db68f
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2019-10-13 14:41:27 +0200

    Updated .gitignore to exclude presentation, data and pycache folders.

--- project/src/utilities.py
+++ project/src/utilities.py
@@ -5,12 +5,12 @@ import numpy as np
 from typing import List, Dict
 from queue import Queue
 
-def read_h5_file(dir_path: str, file_name: str, search_keys: List[str], queue_size: int):
+def read_h5_file(dir_path: str, file_name: str, search_keys: List[str], queue_size: int) -> Dict[str, np.ndarray]:
     file_path = dir_path + '/' + file_name
     assert file_name.lower().endswith('.h5'), 'File is not a h5 file: {}'.format(file_name)
     assert os.path.exists(dir_path), 'Folder does not exist: {}'.format(os.path.abspath(dir_path))
     assert os.path.isfile(file_path), 'File does not exist: {}'.format(os.path.abspath(file_path))
-    
+
     search_keys = [x.lower() for x in search_keys]
     datasets = dict()
     group_queue = Queue(queue_size)
@@ -22,19 +22,23 @@ def read_h5_file(dir_path: str, file_name: str, search_keys: List[str], queue_si
             elif isinstance(value, h5py.Dataset):
                 if key.lower() in search_keys:
                     datasets[key] = value
-
         while not group_queue.empty():
             group = group_queue.get()
+            name = group.name
             for key, value in group.items():
-                print('key: {}, value: {}'.format(key, value))
                 if isinstance(value, h5py.Group):
                     group_queue.put(value)
                 elif isinstance(value, h5py.Dataset):
+                    print('name: {}, key: {}, value: {}'.format(name, key, value))
                     if key.lower() in search_keys:
-                        datasets[key] = value
+                        key = name + '/' + key;
+                        arr = np.empty(shape=value.shape, dtype=value.dtype)
+                        value.read_direct(arr)
+                        datasets[key] = arr
     
     return datasets
 
 if __name__ == "__main__":
-    datasets = read_h5_file('../data', 'ark_20170927_152616_1.h5', ['roll', 'rollrate', 'pitch', 'imu', 'rgbframes'], 500)
-    print(datasets)
+    datasets = read_h5_file('../data', 'ark_20170927_152616_1.h5', ['roll', 'rollrate', 'pitch', 'imu', 'rgbframes', 'timestamp', 'timestampmeasured'], 500)
+    for key, value in datasets.items():
+        print('key: {}, value: {}'.format(key, value.dtype))
