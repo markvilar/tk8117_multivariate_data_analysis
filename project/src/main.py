Implemented PLS inspection. Still need to fix explained variance.

project/src/main.py
@@ -3,10 +3,11 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import PLSRegression
 from mpl_toolkits import mplot3d
 
 from dataloaders import IndianPinesDataloader
-from utilities import print_array_info
+from utilities import print_array_info, create_table, one_hot_encode
 
 def data_calibration(dataloader):
     # Rawdata
@@ -52,14 +53,21 @@ def data_inspection(dataloader, plot: bool):
         plt.ylabel('Radiance [Wm^(-2)sr^(-1)]')
         plt.show()
 
-def pca_inspection(dataloader, plot: bool):
-    # PCA
-    pca = PCA()
-    scores = pca.fit_transform(X)
-    loadings = pca.components_
-    var_ratios = pca.explained_variance_ratio_
+def pls_inspection(dataloader, plot: bool):
+    rads = dataloader.get_rads()
+    labels = dataloader.get_labels()
+    X = create_table(rads)
+    labels = create_table(labels)
+    Y = one_hot_encode(labels)
+    # PLS
+    pls = PLSRegression(6)
+    x_scores, y_scores = pls.fit_transform(X, Y)
+    loadings = pls.x_loadings_
+    # Variance
+    x_var = np.var(x_scores, axis=0)
+    tot_var = np.sum(x_var)
+    var_ratios = x_var / tot_var
     cum_var_ratios = np.cumsum(var_ratios)
-
     if plot:
         # Explained variance plot
         plt.figure(num=3, figsize=(8,6))
@@ -72,7 +80,7 @@ def pca_inspection(dataloader, plot: bool):
 
         # 2D scores plot
         plt.figure(num=4, figsize=(8,6))
-        plt.scatter(scores[:,0], scores[:,1], s=1, c=Y)
+        plt.scatter(x_scores[:,0], x_scores[:,1], s=1, c=labels)
         plt.title('Scores plot')
         plt.xlabel('PC1 ({:.2f}% explained variance)'.format(var_ratios[0]*100))
         plt.ylabel('PC2 ({:.2f}% explained variance)'.format(var_ratios[1]*100))
@@ -80,15 +88,16 @@ def pca_inspection(dataloader, plot: bool):
         # 3D scores plot
         fig = plt.figure(num=5, figsize=(8,6))
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(scores[:,0], scores[:,1], scores[:,2], c=Y)
+        ax.scatter(x_scores[:,0], x_scores[:,1], x_scores[:,2], c=labels)
         ax.set_xlabel('PC1 ({:.2f}% explained variance)'.format(var_ratios[0]*100))
         ax.set_ylabel('PC2 ({:.2f}% explained variance)'.format(var_ratios[1]*100))
         ax.set_zlabel('PC3 ({:.2f}% explained variance)'.format(var_ratios[2]*100))
 
         plt.show()
 
-def data_preprocess(dataloader):
-    raise NotImplementedError 
+def data_preprocess(dataloader, plot: bool):
+    rads = dataloader.get_rads()
+
 
 def lda_classification(dataloader):
     raise NotImplementedError
@@ -103,8 +112,9 @@ def main():
     labels_file = 'indian_pines_gt.mat'
     dataloader = IndianPinesDataloader(dir_path, data_file, cali_file, labels_file)
     data_calibration(dataloader)
-    data_inspection(dataloader, True)
-    pca_inspection(dataloader, True)
+    data_inspection(dataloader, False)
+    data_preprocess(dataloader, True)
+    pls_inspection(dataloader, True)
 
 if __name__ == '__main__':
     main()

project/src/utilities.py
@@ -82,9 +82,7 @@ def one_hot_encode(labels: np.ndarray) -> Tuple[Dict, np.ndarray]:
     ''' One hot encodes labels. 
     arg labels: WxHxP np.array of uints
     return: tuple of dict and np.ndarray '''
-    width, height = labels.shape
-    flat = labels.flatten()
-    n_samples = flat.shape[0]
+    n_samples = len(labels)
     classes = np.unique(labels)
     n_classes = len(classes)
     if n_classes < 256:
@@ -92,6 +90,6 @@ def one_hot_encode(labels: np.ndarray) -> Tuple[Dict, np.ndarray]:
     else:
         data_type = np.uint16
     encoded_labels = np.zeros((n_samples, n_classes), dtype=data_type)
-    encoded_labels[np.arange(n_samples), flat] = 1
-    encoded_labels = encoded_labels.reshape((width, height, n_classes))
+    encoded_labels[np.arange(n_samples), labels] = 1
+    encoded_labels = encoded_labels.reshape((n_samples, n_classes))
     return encoded_labels